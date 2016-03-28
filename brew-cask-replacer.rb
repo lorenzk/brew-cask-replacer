b9ceb60f6e56c1c505d234d7dcd3e3dcf272766a
Lorenz Kitzmann <lorenz@lorenzkitzmann.com>
2016-03-28 20:05:15 +0200

    init

---
+++ brew-cask-replacer.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+Dir.glob('/Applications/*.app').each do |path|
+  next if File.symlink?(path)
+
+  # Remove version numbers at the end of the name
+  app = path.slice(14..-1).sub(/.app\z/, '').sub(/ \d*\z/, '')
+  searchresult = `brew cask search #{app}`
+  puts searchresult
+
+  next unless searchresult =~ /Exact match/
+
+  token = searchresult.split("\n")[1]
+  puts "Installing #{token}..."
+  begin
+    FileUtils.mv(path, File.expand_path('~/.Trash/'))
+  rescue Errno::EPERM, Errno::EEXIST
+    puts "ERROR: Could not move #{path} to Trash"
+    next
+  end
+  puts `brew cask install #{token} --appdir=/Applications`
+end
